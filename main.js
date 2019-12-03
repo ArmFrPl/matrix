7b1a46090038b988543c71f4512a688f7b59d4bc
ArmFrPl <ehakobkekhvyan7@gmail.com>
2019-12-03 11:24:27 +0400

    bug fixed

--- main.js
+++ main.js
@@ -13,5 +13,9 @@ let mat2 = [[7, 6, 3],
   [8, 5, 2],
   [9, 4, 1]];
 
-console.log(print(multiplyMatrices(mat1, mat2)))  ;
+if(typeof multiplyMatrices(mat1, mat2) === 'string'){
+  console.log(multiplyMatrices(mat1, mat2));
+}else{
+  console.log(print(multiplyMatrices(mat1, mat2)))  ;
+}
 console.log(print(mat));
